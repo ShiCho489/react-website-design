c274599c426be47c5248f8b9465a7da945f0b1c0
Shivani Choudhary <choudhary.shivani489@gmail.com>
2023-05-15 14:18:05 +0300

    animation added

--- react-web-design/src/components/AnimatedShapes.js
+++ react-web-design/src/components/AnimatedShapes.js
@@ -12,12 +12,11 @@ const Square = styled.div`
     
 
     @keyframes square {
-        0%   {background-color:red; left:0px; top:50px;}
-       15%  {background-color:yellow; left:500px; top:0px;}
-       25%  {background-color:plum; left: 500px; bottom: 500px;}
-       50%  {background-color:blue; left:200px; top:100px; }
-       75%  {background-color:green; left:50px; top:200px;}
-       100% {background-color:red; left:0px; top:0px;}
+      0%   {background-color:red; left:0px; top:0px;}
+      25%  {background-color:yellow; left:200px; top:0px;}
+      50%  {background-color:blue; left:200px; top:200px;}
+      75%  {background-color:green; left:0px; top:200px;}
+      100% {background-color:red; left:0px; top:0px;}
     }
 
 `;
